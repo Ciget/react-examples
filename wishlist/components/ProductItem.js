2e1c17555c8839b0ebc11933efe868eaf2dfebce
Victor Bilokin <ciget@yandex.ru>
2016-05-14 01:54:59 +0300

    add wishbox

--- wishlist/components/ProductItem.js
+++ wishlist/components/ProductItem.js
@@ -1,33 +1,32 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux'
+import {addToCompare} from '../actions'
 
 class ProductItem extends Component{
 
   render(){
     const product = this.props.product;
     return (
-      <div key={product.id} className={''}>
-        <div className="row"> <div className="col-sm-6 col-md-4"> 
-          <div className="thumbnail">
-           <img data-src="holder.js/100%x200" alt="100%x200" style={{height: 200, width: 100, display: 'block'}}
-            data-holder-rendered="true"/>
-            <div className="caption"> 
-            <h3>Thumbnail label</h3> <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p> <p><a href="#" className="btn btn-primary" role="button">Button</a> <a href="#" className="btn btn-default" role="button">Button</a></p> </div> 
-          </div> 
-        </div> 
-        <div className="col-sm-6 col-md-4"> <div className="thumbnail"> 
-        <img data-src="holder.js/100%x200" alt="100%x200" style={{height: 200, width: 100, display: 'block'}} 
-        data-holder-rendered="true"/> 
-          <div className="caption"> 
-            <h3>Thumbnail label</h3> <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p> <p><a href="#" className="btn btn-primary" role="button">Button</a> <a href="#" className="btn btn-default" role="button">Button</a></p> </div> 
-          </div>
-        </div> 
-        <div className="col-sm-6 col-md-4"> <div className="thumbnail"> 
-        <img data-src="holder.js/100%x200" alt="100%x200" style={{height: 200, width: 100, display: 'block'}}  
-        data-holder-rendered="true"/> <div className="caption"> <h3>Thumbnail label</h3> <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p> <p><a href="#" className="btn btn-primary" role="button">Button</a> <a href="#" className="btn btn-default" role="button">Button</a></p> </div> </div> </div> 
+      <div key={product.id} className={'container-fluid'}>
+        <div className="row">
+        <div className="col-md-4"> <div className="thumbnail"> 
+         <div className="caption"> <h4>{product.name}</h4> <p>{product.description}</p> <p>
+         <a href="#" className="btn btn-primary" onClick={this.Compare} role="button">Compare</a> </p> </div> </div> </div> 
       </div>
       </div>
     )
   }
 }
-export default connect()(ProductItem);
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  console.log(ownProps);
+  return {
+    Compare: (e) => {
+      dispatch(addCompare(1, 'asds'))
+    }
+  }
+}
+
+export default connect(
+  null,
+  mapDispatchToProps)(ProductItem);
