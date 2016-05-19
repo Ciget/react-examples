a6546d84ab697b8e57c4e341e327f8df9b0a0a58
Victor Bilokin <Victor_Bilokin@epam.com>
2016-05-19 12:14:31 +0300

    implement async middleware

---
+++ middleware/src/middlewares/async.js
@@ -0,0 +1,14 @@
+export default function({ dispatch})
+{
+     return next=>action=>{
+         
+         //if action does not have payload//or the payload does not have a .then property - we dont care about it
+         if (!action.payload || !action.payload.then) return next(action);
+         
+         action.payload.then(function(response){
+             console.log(response);
+             const newAction =  {...action, payload: response};
+             dispatch(newAction);
+         })
+    };
+}
