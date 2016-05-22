1a6a9c99165c4733d1396817ef586fb001164138
Victor Bilokin <ciget@yandex.ru>
2016-05-23 00:10:46 +0300

    completed signin and signup

--- auth/server/router.js
+++ auth/server/router.js
@@ -1,5 +1,14 @@
+const Authentication = require('./controllers/authentication');
+const passport = require('passport');
+const passportService = require('./services/passport');
+
+const requireAuth =   passport.authenticate('jwt', {session: false});
+const requireSignin = passport.authenticate('local', {session: false});
+
 module.exports = function(app){
-    app.get('/', function(req, res, next){
-        res.send(['asdasd', 'asd']);
-    });
+    // app.get('/', requireAuth, function(req, res){
+    //     res.send({hi:'asd'});
+    // });
+    app.post('/signup',  Authentication.signup);
+    app.post('/signin', requireSignin, Authentication.signin);
 }
